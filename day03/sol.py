8c80aa7c0211f487c63ff34a6e2956e05a69cd7f
jartigag <javi@artiga.es>
2023-12-12 09:31:42 +0100

    d3p2

--- day03/sol.py
+++ day03/sol.py
@@ -22,5 +22,29 @@ def part1(engine_schematic: list) -> list:
 
     return nums
 
+def part2(engine_schematic: list) -> list:
+    gears = dict()
+    nums = []
+
+    gears_regex = r'\*'
+    for i, line in enumerate(engine_schematic):
+        for m in re.finditer(gears_regex, line):
+            gears[(i, m.start())] = []
+
+    numbers_regex = r'\d+'
+    for i, line in enumerate(engine_schematic):
+        for m in re.finditer(numbers_regex, line):
+            for r in range(i-1, i+2):
+                for n in range(m.start()-1, m.end()+1):
+                    if (r, n) in gears:
+                        gears[(r, n)].append(int(m.group()))
+
+    for v in gears.values():
+        if len(v) == 2:
+            nums.append(v[0]*v[1])
+
+    return nums
+
 if __name__ == '__main__':
     print( sum(part1(input)) )
+    print( sum(part2(input)) )
